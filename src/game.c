8d4733b88b6dee08ec57b6a09eb58c204f8d1b11
aznoH333 <glentik23@gmail.com>
2026-05-23 12:22:04 +0200

    got basic rendering working

--- src/game.c
+++ src/game.c
@@ -45,10 +45,8 @@ Texture2D* getSprite(char* spriteName) {
 //------------------------------------------------------------------------------------
 // Sprite drawing
 //------------------------------------------------------------------------------------
-
 #define MAX_TEXTURE_LAYERS 8
 #define MAX_TEXTURES_PER_LAYER 1024
-
 
 typedef struct {
 	Texture2D* texture;
@@ -60,16 +58,6 @@ typedef struct {
 } DrawArguments;
 
 void drawSpriteFromArguments(DrawArguments* arguments) {
-
-	// void DrawTexturePro(Texture2D texture, Rectangle source, Rectangle dest, Vector2 origin, float rotation, Color tint);
-
-	//DrawTexturePro(
-	//	texture, 
-	//	(Rectangle){0,0, texture.width, texture.height}, 
-	//	(Rectangle){x, y, texture.width, texture.height},
-	//	(Vector2) {texture.width / 2.0f, texture.height / 2.0f},
-	//	0.0f, WHITE);
-
 	DrawTexturePro(
 		*arguments->texture,
 		arguments->source,
@@ -86,16 +74,16 @@ GenArrayDefinition(DrawArguments, MAX_TEXTURES_PER_LAYER,DrawArgumentArray)
 DrawArgumentArray drawQueue[MAX_TEXTURE_LAYERS] = {0};
 
 
-void spr(char* spriteName, float x, float y, int layer) {
+void spr(char* spriteName, float x, float y, float width, float height, bool flipX, bool flipY, int layer) {
 	DrawArgumentArray* target = &drawQueue[layer];
 	
 	Texture2D* texture = getSprite(spriteName); 
 
 	DrawArguments args = {
 		texture, 
-		(Rectangle){0,0, texture->width, texture->height}, 
-		(Rectangle){x, y, texture->width, texture->height},
-		(Vector2) {texture->width / 2.0f, texture->height / 2.0f},
+		(Rectangle){0, 0, texture->width * -1, texture->height}, 
+		(Rectangle){x, y, texture->width * width, texture->height * height},
+		(Vector2) {texture->width / 2.0f * width, texture->height / 2.0f * height},
 		0.0f, WHITE
 	};
 
@@ -120,11 +108,8 @@ void renderTextures() {
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-
 int main(void)
 {
-    
-
 	SetTraceLogLevel(LOG_WARNING); 
     const int screenWidth = 800;
     const int screenHeight = 400;
@@ -152,8 +137,8 @@ int main(void)
 			SwitchResolution(0, 0, true);
 		}
 
-		spr("debug_0001", poo++, 10, 0);
-
+		spr("debug_0001", 100, 10, 2, 1, false, false, 0);
+		spr("debug_0001", 100, 26, 2, 1, true, false, 0);
 		BeginTextureRendering();
 
 		ClearBackground(BLUE);
